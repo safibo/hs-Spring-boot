e47a6d1c4f4a5a8d248abac70c9730d88d2feabc
Eric Huang <75153136@qq.com>
2020-08-21 23:37:52 +0800

    Test

---
+++ Spring Boot/src/main/java/com/eric/HelloWorldMainApplication.java
@@ -0,0 +1,14 @@
+package com.eric;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldMainApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(HelloWorldMainApplication.class,args);
+    }
+}
